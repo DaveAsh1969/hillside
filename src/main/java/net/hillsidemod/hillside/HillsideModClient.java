2964569a827c636e379a86f2835b458830d81f12
DavidAshworth1969 <david.hillsidechurch@outlook.com>
2023-05-05 11:19:56 -0500

    Initial commit

---
+++ src/main/java/net/hillsidemod/hillside/HillsideModClient.java
@@ -0,0 +1,10 @@
+package net.hillsidemod.hillside;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HillsideModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
